0df4777397368514f9bc9c269bf401b7e904d348
blaize stewart <me@blaize.net>
2016-12-20 20:30:36 -0500

    Added files

---
+++ install-agents.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Detects the user
+user=$(whoami)
+
+#Loops over each of the swarm nodes and installs the driver on each node
+docker -H 127.0.0.1:2375 info | grep -oP '(?:[0-9]{1,3}\.){3}[0-9]{1,3}' | while read -r ip ; do
+    echo "Processing $ip"
+    scp install-local.sh azurefile azurefile.conf $user@$ip:~/
+	ssh $user@$ip sudo sh ~/install-local.sh
+done
