92fa40b62082bd6a12a7f9c0482dea6fa204ccd1
Bevan Wentzel <bevanw@live.co.za>
2017-05-30 10:32:39 +0200

    Fixed file name in kubernetes script

--- install-agents-kubernetes.sh
+++ install-agents-kubernetes.sh
@@ -5,7 +5,7 @@ user=$(whoami)
 
 kubectl get nodes | grep -o 'k8s-agent-.*-[0-9]*' | while read -r agent; do
     echo "Processing $agent"
-    scp -o "StrictHostKeyChecking no" local.sh $user@$agent:~/
-    ssh -o "StrictHostKeyChecking no" -n $user@$agent sudo sh ~/local.sh
+    scp -o "StrictHostKeyChecking no" install-local.sh $user@$agent:~/
+    ssh -o "StrictHostKeyChecking no" -n $user@$agent sudo sh ~/install-local.sh
     echo "Done $agent"
 done
